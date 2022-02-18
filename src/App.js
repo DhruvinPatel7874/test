a512b2291f26af4cee2e11b0ccff9b8fedcd8da1
DhruvinPatel7874 <98503474+DhruvinPatel7874@users.noreply.github.com>
2022-02-18 12:43:19 +0530

    Update App.js
    Second changes from git

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Dhruvin
+          Learn React Dhruvin ABC
         </a>
       </header>
     </div>
